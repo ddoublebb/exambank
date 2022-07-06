5dd9de171030f67516e832f08eefaaeaa0dedd3a
ddoublebb <ddoublebb@github.com>
2022-07-06 21:42:46 +0700

    addpages

---
+++ pages/TEST-1/test.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function () {
+    'use strict'
+
+    // $(".writBackG").css('background-color', 'blue');
+    // $(".writBackG").css('background-color', 'blue');
+    // $(".writBackG").css('color', 'red');
+    // $(".writBackG").css('font-size', 'xx-large');
+
+    // let x = 15;
+
+    // let y = 10;
+
+    // if (x === y) {
+    //     console.log("เงื่อนไขแรก")
+    //     $(".writBackG").addClass("calltest");
+    // } else {
+    //     console.log("เงื่อนไขสอง")
+    //     $(".writBackG").css('background-color', 'blue');
+    //     $(".writBackG").css('color', 'red');
+    //     $(".writBackG").css('font-size', 'xx-large');
+    // }
+
+
+    // คำตอบข้อ 9
+    console.log("คำตอบข้อ 9 A : " + $('#numberOne').val());
+    // หรือ
+    console.log("คำตอบข้อ 9 B : " + document.getElementById('numberOne').value);
+
+
+    // คำตอบข้อ 10
+    alert("คำตอบข้อ 10 A : " + $('#numberOne2').val());
+    // หรือ
+    alert("คำตอบข้อ 9 B : " + document.getElementById('numberOne2').value);
+
+
+
+
+
+});
+
